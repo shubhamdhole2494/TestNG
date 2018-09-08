a5f27c2fa1e1c07fd634954c87031bae7ad1fbc4
Hp <Hp@192.168.0.8>
2018-09-08 11:02:05 +0530

    First commit

---
+++ com.Mobiquity.TestNG/src/test/java/com/Mobiquity/com/Mobiquity/TestNG/TestAnnotation.java
@@ -0,0 +1,24 @@
+package com.Mobiquity.com.Mobiquity.TestNG;
+import org.testng.annotations.*;
+
+//import appcode.SomeClassToTest;
+
+public class TestAnnotation {
+	
+	@Test
+	public void testMethod1() {
+		//SomeClassToTest obj = new SomeClassToTest();
+		//int result = obj.sumNumbers(1, 2);
+		System.out.println("Running Test -> testMethod1");
+	}
+	
+	@Test
+	public void testMethod2() {
+		System.out.println("Running Test -> testMethod2");
+	}
+	
+	@Test
+	public void testMethod3() {
+		System.out.println("Running Test -> testMethod3");
+	}
+}
